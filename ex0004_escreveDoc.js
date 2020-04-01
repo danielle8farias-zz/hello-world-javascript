3463e1a5c605a2f2d3c1352a13b2afe57b5d5702
Danielle <danielle8farias@gmail.com>
2020-03-31 21:25:04 -0300

    escrevendo no documento com JS

---
+++ ex0004_escreveDoc.js
@@ -0,0 +1,14 @@
+document.write('Escrevendo no corpo do documento');
+// quebra de linha tag br
+document.write('<br>');
+document.write(50);
+document.write('<br>');
+// concatenação de string
+document.write('100' + 50);
+document.write('<br>');
+document.write(100 + '50');
+document.write('<br>')
+// soma de dois inteiros
+document.write(100 + 50)
+document.write('<br>')
+document.write('Estudando programação<br>Aprendendo Javascript')
