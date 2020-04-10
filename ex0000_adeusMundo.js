6f1754c304e68978843d32092c80177c10f812b6
Danielle <danielle8farias@gmail.com>
2020-04-10 12:01:57 -0300

    programa 'adeus mundo'

---
+++ ex0000_adeusMundo.js
@@ -0,0 +1,14 @@
+/* comentário em JS */
+
+// abre tag h1
+document.write('<h1>')
+// escreve na tag h1
+document.write('Adeus, mundo!')
+// fecha tag h1
+document.write('</h1>')
+// abre tag p
+document.write('<p>')
+// escreve na tag p
+document.write('Esse mundo é muito cruel')
+// fecha a tag p
+document.write('</p>')
