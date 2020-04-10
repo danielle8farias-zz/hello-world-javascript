70e07c1d82e7d804b3f44317fc819cfb626273bd
Danielle <danielle8farias@gmail.com>
2020-04-10 14:56:48 -0300

    manipulando string

---
+++ ex0005_manipulaNome/ex0005_manipulaNome.js
@@ -0,0 +1,5 @@
+let nome = prompt('Qual é o seu nome?')
+
+document.write(`<p>Seu nome tem ${nome.length} letras. </p>`)
+document.write(`<p>Seu nome em maiúsculo: ${nome.toUpperCase()} </p>`)
+document.write(`<p>Seu nome em minúsculo: ${nome.toLowerCase()} </p>`)
