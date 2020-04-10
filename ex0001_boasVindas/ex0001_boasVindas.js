2988429efe0d45ac565531553f6ec707cbaa58b2
Danielle <danielle8farias@gmail.com>
2020-04-10 12:15:45 -0300

    deseja boas vindas ao usuário

---
+++ ex0001_boasVindas/ex0001_boasVindas.js
@@ -0,0 +1,9 @@
+let nome = prompt('Qual o seu nome?')
+
+
+// abre tag h1
+document.write('<h1>')
+// escreve na tag h1
+document.write('Bem-vinda, ' + nome + '!')
+// fecha tag h1
+document.write('</h1>')
