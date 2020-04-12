0305d773bc8d99614454923fee07d75414bb066e
Danielle <danielle8farias@gmail.com>
2020-04-12 14:30:57 -0300

    funções

---
+++ ex0009_funcao/ex0009_funcao.js
@@ -0,0 +1,16 @@
+function clicar(){
+    let area_clique = document.getElementById('area')
+    area_clique.innerText = 'Clicou!'
+    area_clique.style.background = 'red'
+}
+
+function entrar(){
+    let area_clique = document.getElementById('area')
+    area_clique.innerText = 'Entrou!'
+}
+
+function sair(){
+    let area_clique = document.getElementById('area')
+    area_clique.innerText = 'Saiu!'
+    area_clique.style.background = '#7736a1'
+}
