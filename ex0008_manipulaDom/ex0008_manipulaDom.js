f5c763bd469795d4fe4bf0fece6463cd390703e5
Danielle <danielle8farias@gmail.com>
2020-04-12 12:15:51 -0300

    manipulando dados do DOM

---
+++ ex0008_manipulaDom/ex0008_manipulaDom.js
@@ -0,0 +1,37 @@
+// mostrando config de caractere
+document.write(document.charset + '<br/>')
+
+// mostrando navegador
+document.write(navigator.appName + '<br/>')
+
+// mostrando url
+document.write(document.URL + '<br/>')
+
+// pegando a tag body
+let corpo = document.body
+// mudando estilo da tag
+corpo.style.background = 'black'
+
+// pegando a primeira tag p
+let p1 = document.getElementsByTagName('p')[0]
+document.write(p1.innerText + '<br/>')
+
+// pegando a segunda tag p
+let p2 = document.getElementsByTagName('p')[1]
+// mostrando apenas texto sem formatação
+document.write(p2.innerText + '<br/>')
+// mostrando texto com formatação
+document.write(p2.innerHTML + '<br/>')
+
+// mudando a cor da variável p1
+p1.style.color = 'blue';
+
+// pegando elemento por id
+var d = document.getElementById('msg')
+d.style.background = 'green'
+// mudando o conteúdo do id
+d.innerText = 'Estou aguardando...'
+
+// pegando por seletor
+var c = document.querySelector('div#clique')
+c.style.color = 'blue'
