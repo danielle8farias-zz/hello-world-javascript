63a4757df1824a5ea369a8234ca1be632d60f4a6
Danielle <danielle8farias@gmail.com>
2020-04-13 15:52:38 -0300

    média entre duas notas

---
+++ ex0011_mediaDuasNotas/ex0011_mediaDuasNotas.js
@@ -0,0 +1,21 @@
+function media(){
+    var texto1 = document.getElementById('nota1')
+    var texto2 = document.getElementById('nota2')
+    var resposta = document.getElementById('media')
+
+    var nota1 = Number(texto1.value)
+    var nota2 = Number(texto2.value)
+    var media = (nota1 + nota2)/2
+
+    if(media < 5){
+        resposta.innerHTML = `Média = ${media} Estudante reprovado!`
+    }
+    else {
+        if(media >= 5 && media <= 7){
+            resposta.innerHTML = `Média = ${media} Estudante em recuperação!`
+        }
+        else {
+            resposta.innerHTML = `Média = ${media} Estudante Aprovado!`
+        }
+    }
+}
