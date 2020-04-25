45d14777a74240490f161bb8f7e9dbe941f82af3
Danielle <danielle8farias@gmail.com>
2020-04-25 17:48:52 -0300

    comentários

--- ex0001_boasVindas/ex0001_boasVindas.js
+++ ex0001_boasVindas/ex0001_boasVindas.js
@@ -1,9 +1,11 @@
+/** criando variável */
+/** recebendo valor digitado */
 let nome = prompt('Qual o seu nome?')
-
 
 // abre tag h1
 document.write('<h1>')
 // escreve na tag h1
+// + concatenação de strings
 document.write('Bem-vinda, ' + nome + '!')
 // fecha tag h1
 document.write('</h1>')
