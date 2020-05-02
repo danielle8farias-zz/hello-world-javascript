aaf1f37cb1bdd73051b7ef74ef7bedcee86f5ba0
Danielle <danielle8farias@gmail.com>
2020-05-02 20:35:00 -0300

    adicionando header

--- ex0004_numeroAntesEDepois/ex0004_numeroAntesEDepois.js
+++ ex0004_numeroAntesEDepois/ex0004_numeroAntesEDepois.js
@@ -1,11 +1,12 @@
-//criando variável
-//pegando elemento pelo id do html
-let num = document.getElementById('num')
-
-//atribuindo variável para a resposta
-let res = document.getElementById('res')
-
+//função é chamada ao clicar no botão
 function enviar() {
+    //criando variável
+    //pegando elemento pelo id do html
+    let num = document.getElementById('num')
+
+    //atribuindo variável para a resposta
+    let res = document.getElementById('res')
+
     //convertendo valor para número
     //subtraindo 1 para calcular o valor anterior
     let ant = Number(num.value) - 1
