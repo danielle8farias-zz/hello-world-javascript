53e9f9ff2a81d5358d6c114c3e920f1943dfae4d
Danielle <danielle8farias@gmail.com>
2020-05-02 20:36:39 -0300

    organizando arquivos

---
+++ exercicio_js/ex0004_numeroAntesEDepois/ex0004_numeroAntesEDepois.js
@@ -0,0 +1,19 @@
+//função é chamada ao clicar no botão
+function enviar() {
+    //criando variável
+    //pegando elemento pelo id do html
+    let num = document.getElementById('num')
+
+    //atribuindo variável para a resposta
+    let res = document.getElementById('res')
+
+    //convertendo valor para número
+    //subtraindo 1 para calcular o valor anterior
+    let ant = Number(num.value) - 1
+    //somando 1 para calcular o valor posterior
+    let suc = Number(num.value) + 1
+
+    //escrevendo no html as respostas
+    res.innerHTML = `<p>Antecessor: ${ant}</p>`
+    res.innerHTML += `<p>Sucessor: ${suc}</p>`
+}
