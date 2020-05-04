8af7fa1f174425dc449e6b0bff89015c4ba894c0
Danielle <danielle8farias@gmail.com>
2020-05-04 18:01:04 -0300

    refatorando código

--- ex0011_mediaDuasNotas/ex0011_mediaDuasNotas.js
+++ ex0011_mediaDuasNotas/ex0011_mediaDuasNotas.js
@@ -1,19 +1,27 @@
+//função é chamada ao clicar no botão
 function media(){
-    var texto1 = document.getElementById('nota1')
-    var texto2 = document.getElementById('nota2')
-    var resposta = document.getElementById('media')
+    //criando variável
+    //pegando elemento pelo id do html
+    let nota1 = document.getElementById('nota1')
+    let nota2 = document.getElementById('nota2')
+    let res = document.getElementById('res')
 
-    var nota1 = Number(texto1.value)
-    var nota2 = Number(texto2.value)
-    var media = (nota1 + nota2)/2
+    //value pega o valor da variável
+    //Number converte para número
+    //atribuindo resultado da operação a variável média
+    let media = ( Number(nota1.value) + Number(nota2.value) )/2
 
+    //verificando se a média é menor do que 5
     if(media < 5){
-        resposta.innerHTML = `Média = ${media} Estudante reprovado!`
+        //escrevendo no doc html
+        res.innerHTML = `<p>Média = ${media} Estudante reprovado!</p>`
     }
+    //(senão, se) verificando se a média está entre 5 e 7 
     else if(media >= 5 && media <= 7) {
-            resposta.innerHTML = `Média = ${media} Estudante em recuperação!`
+            res.innerHTML = `<p>Média = ${media} Estudante em recuperação!</p>`
     }
+    //entra nesse bloco de comando, caso nenhuma das expressõe acima seja verdadeira
     else {
-        resposta.innerHTML = `Média = ${media} Estudante Aprovado!`
+        res.innerHTML = `<p>Média = ${media} Estudante Aprovado!</p>`
     }
 }
