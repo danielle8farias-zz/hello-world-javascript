e1d4068d21a683388932641c7403859330843641
Danielle <danielle8farias@gmail.com>
2020-05-06 18:49:03 -0300

    comentários

--- exercicio_js/ex0010_eventListener/ex0010_eventListener.js
+++ exercicio_js/ex0010_eventListener/ex0010_eventListener.js
@@ -1,18 +1,27 @@
-var areaClique = document.getElementById('area')
+//pegando elemento pelo id do html
+//criando variável
+let areaClique = document.getElementById('area')
 
+//addEventListener() método que captura e manipula um elemento específico
+//addEventListener(evento, função)
 areaClique.addEventListener('click', clicar)
 areaClique.addEventListener('mouseenter', entrar)
 areaClique.addEventListener('mouseout', sair)
 
+//função chamada ao clicar no quadrado
 function clicar(){
+    //escrevendo texto no html
     areaClique.innerText = 'Clicou!'
+    //mudando cor de fundo
     areaClique.style.background = 'red'
 }
 
+//chama a função através do evento onmouseenter no DOM
 function entrar(){
     areaClique.innerText = 'Entrou!'
 }
 
+//chama a função através do evento onmouseout no DOM
 function sair(){
     areaClique.innerText = 'Saiu!'
     areaClique.style.background = '#7736a1'
