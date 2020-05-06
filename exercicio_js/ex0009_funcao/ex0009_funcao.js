16f12132bd9177266cd7c15e0697e76e6e6efc1a
Danielle <danielle8farias@gmail.com>
2020-05-06 18:12:21 -0300

    comentando código

--- exercicio_js/ex0009_funcao/ex0009_funcao.js
+++ exercicio_js/ex0009_funcao/ex0009_funcao.js
@@ -1,14 +1,21 @@
+//função chamada ao clicar no quadrado
 function clicar(){
+    //pegando elemento pelo id do html
+    //criando variável
     let area_clique = document.getElementById('area')
+    //escrevendo texto no html
     area_clique.innerText = 'Clicou!'
+    //mudando cor de fundo
     area_clique.style.background = 'red'
 }
 
+//chama a função através do evento onmouseenter no DOM
 function entrar(){
     let area_clique = document.getElementById('area')
     area_clique.innerText = 'Entrou!'
 }
 
+//chama a função através do evento onmouseout no DOM
 function sair(){
     let area_clique = document.getElementById('area')
     area_clique.innerText = 'Saiu!'
