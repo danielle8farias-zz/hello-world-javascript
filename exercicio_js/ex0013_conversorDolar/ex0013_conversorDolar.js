1d396e23c5858272134c161420bd7cb5169a7e7c
Danielle <danielle8farias@gmail.com>
2020-05-08 17:20:13 -0300

    reorganizando arquivos

---
+++ exercicio_js/ex0013_conversorDolar/ex0013_conversorDolar.js
@@ -0,0 +1,16 @@
+//função é chamada ao clicar no botão
+function calcular() {
+    //criando variável
+    //pegando elemento pelo id do html
+    let real = document.getElementById('valor-real')
+    let res = document.querySelector('div#res')
+
+    //value pega o valor da variável
+    //Number converte para número
+    //atribuindo resultado da operação a variável dolar
+    let dolar = Number(real.value) / 5.25
+
+    //toFixed() para fixar o número de casas decimais
+    //escrevendo no doc html
+    res.innerHTML = `<p>U$ ${dolar.toFixed(2)}</p>`
+}
