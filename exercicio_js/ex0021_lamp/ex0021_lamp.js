87b817412a1e85472c1f0b36828ad872c0bbe1d4
Danielle <danielle8farias@gmail.com>
2020-06-25 13:15:45 -0300

    valor do botão

--- exercicio_js/ex0021_lamp/ex0021_lamp.js
+++ exercicio_js/ex0021_lamp/ex0021_lamp.js
@@ -1,13 +1,16 @@
 function clicar() {
     let desligado = document.getElementById('lamp-off')
     let ligado = document.getElementById('lamp-on')
+    let btn = document.querySelector('input')
 
     if (desligado.style.display == 'none') {
         desligado.style.display = 'block'
         ligado.style.visibility = 'hidden'
+        btn.value = 'ON'
     }
     else {
         desligado.style.display = 'none'
         ligado.style.visibility = 'visible'
+        btn.value = 'OFF'
     }
 }
