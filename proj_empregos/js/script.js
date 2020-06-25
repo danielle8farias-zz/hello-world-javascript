93bf252989256769460df9c42c5fe1f19fec3a6c
Danielle <danielle8farias@gmail.com>
2020-06-25 20:03:29 -0300

    página de procura de emprego para devs

---
+++ proj_empregos/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    var btnMenuMobile = $('.btn-menumobile');
+
+    $(btnMenuMobile).on('click', function(){
+        $('.nav-container ul').toggleClass('open');
+    });
+
+});
---
+++ proj_empregos/readme.md
@@ -0,0 +1,3 @@
+# Treino de HTML e CSS
+
+* [Hora de Codar](https://www.youtube.com/playlist?list=PLnDvRpP8Bnez2LJGshXKtid2f-aUkFOqM)
