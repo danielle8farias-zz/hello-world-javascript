27449efd834ec5e7b6501c7540952186c635f37e
Danielle <danielle8farias@gmail.com>
2020-06-30 13:08:59 -0300

    função que calcula o preço total

--- proj_glass/script.js
+++ proj_glass/script.js
@@ -1,3 +1,9 @@
 function mudarFoto(foto) {
     document.getElementById('icone').src =  foto
 }
+
+function calcularTotal() {
+    let quant = parseInt(document.getElementById('cQtd').value)
+    total = quant * 1500
+    document.getElementById('cTot').value = total
+}
