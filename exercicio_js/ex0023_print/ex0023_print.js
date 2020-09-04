c13ebe867b7d90c4bdcf33d43971f1a75277813b
Danielle <danielle8farias@gmail.com>
2020-09-04 19:14:27 -0300

    script para disparo de msg

--- exercicio_js/ex0023_print/ex0023_print.js
+++ exercicio_js/ex0023_print/ex0023_print.js
@@ -1,4 +1,9 @@
 //função é chamada ao clicar no botão
 function enviar() {
+    let res = document.getElementById('res')
 
+    res.innerHTML = `<p>Adeus, mundo!</p>
+    <p>Toda criança começa como um cientista nato. 
+    Nós é que tiramos isso delas. 
+    Só umas poucas passam pelo sistema com sua admiração e entusiasmo pela ciência intactos.</p>`
 }
