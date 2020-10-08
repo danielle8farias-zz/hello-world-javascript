71d2800d7bb2d5a6d165dda080f6af9da6747107
Danielle <danielle8farias@gmail.com>
2020-10-08 15:39:20 -0300

    retirando projeto

--- proj_empregos/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    
-    var btnMenuMobile = $('.btn-menumobile');
-
-    $(btnMenuMobile).on('click', function(){
-        $('.nav-container ul').toggleClass('open');
-    });
-
-});
--- proj_glass/script.js
+++
@@ -1,9 +0,0 @@
-function mudarFoto(foto) {
-    document.getElementById('icone').src =  foto
-}
-
-function calcularTotal() {
-    let quant = parseInt(document.getElementById('cQtd').value)
-    total = quant * 1500
-    document.getElementById('cTot').value = total
-}
